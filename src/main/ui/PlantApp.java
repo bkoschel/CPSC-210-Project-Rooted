4064b4cf28a9ba29461c1fd4e38f44e4864cb026
bkoschel <67294728+bkoschel@users.noreply.github.com>
2022-02-09 14:04:27 -0800

    Task 5: start of console program

--- src/main/ui/PlantApp.java
+++ src/main/ui/PlantApp.java
@@ -3,36 +3,79 @@ package ui;
 import model.Plant;
 import model.Plants;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 // Plant organizer application
 public class PlantApp {
-    private Plant name;
-    private Plant type;
-    private Plant status;
-    private Plant water;
+    private Plant plant;
+    private Plant userPlant;
     private Plants myPlants;
     private Scanner input;
+    private boolean exit;
 
-    private PlantApp() {
+    PlantApp() {
         runPlantApp();
     }
 
     private void runPlantApp() {
-        boolean keepGoing = true;
+        exit = true;
         String command = null;
+        input = new Scanner(System.in);
+        System.out.println("Welcome to Rooted!");
 
 
+        init();
+
+        while (exit) {
+            displayMenu();
+            command = input.next();
+
+            processInput(command);
+        }
+        System.out.println("Goodbye!");
     }
 
-    // EFFECTS: displays menu of options to user
-    private void displayHomeMenu() {
-        System.out.println("\nSelect from:");
-        System.out.println("\t1 -> add plant");
-        System.out.println("\t2 -> view collection");
-        System.out.println("\t3 -> view plant");
-        System.out.println("\t4 -> remove plant");
-        System.out.println("\t5 -> quit");
+    private void init() {
+        myPlants = new Plants();
+    }
+
+    private void processInput(String command) {
+        if (command.equals("1")) {
+            displayPlants();
+        } else if (command.equals("2")) {
+            addNewPlant();
+        } else if (command.equals("3")) {
+            removeYourPlant(userPlant);
+        } else if (command.equals("4")) {
+            exit = false;
+        } else {
+            System.out.println("Selection invalid");
+        }
+    }
+
+    private void removeYourPlant(Plant userPlant) {
+    }
+
+    private void addNewPlant() {
+    }
+
+    private void displayPlants() {
+        System.out.println("You currently have " + myPlants.getNumberOfPlantsInCollection()
+                            + " in your collection\n");
+        System.out.println(myPlants.getListOfPlantNames());
+        nameChange();
+    }
+
+    private void nameChange() {
+    }
+
+    private void displayMenu() {
+        System.out.println("\nSelect an option: ");
+        System.out.println("\n\t1 - View house plant collection");
+        System.out.println("\n\t2 - Add new plant");
+        System.out.println("\n\t3 - Remove a plant from the collection");
+        System.out.println("\n\t4 - Exit Rooted");
     }
 
 }
